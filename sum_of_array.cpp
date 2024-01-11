75dcd6c8f75a80d047fdb7e6822c7aae1b8cda49
Riajain05 <100400846+Riajain05@users.noreply.github.com>
2024-01-11 14:22:09 +0530

    Add files via upload

---
+++ sum_of_array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int arraySum(int arr[], int n){
+
+    int sum = 0;
+    for (int i =0; i<n; i++){
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+int main(){
+     int size;
+     cin >> size;
+     
+     int arr[100];
+
+     for(int i=0; i<size; i++){
+        cin >> arr[i];
+     }
+
+     cout << "sum of array element is: "<< arraySum(arr , size);
+     
+     return 0;
+}
